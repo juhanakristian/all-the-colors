df04a33221c59b9d250aa0429559ed4168bded83
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2021-11-10 21:44:18 +0200

    added hsl

--- components/Color.jsx
+++ components/Color.jsx
@@ -7,13 +7,16 @@ export default function Color({ name, value, hsl }) {
 
   return (
     <div
-      className="border-0 border-gray-100 shadow-md w-60 h-32 flex flex-col overflow-hidden rounded-md hover:scale-105 transition-transform"
+      className="border-0 border-gray-100 shadow-md w-60 h-32 flex flex-col overflow-hidden rounded-md hover:scale-105 transition-transform cursor-pointer"
       style={{ backgroundColor: value }}
       onClick={handleClick}
     >
       <div className="bg-gray-700 text-white text-xs p-1 flex lowercase">
         <span>{name}</span>
-        {/* <span>{`HSL: ${hsl.h} ${hsl.s} ${hsl.l}`}</span> */}
+      </div>
+      <div className="flex justify-between text-xs bg-gray-700 bg-opacity-40 text-white p-1">
+        <span>{`hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`}</span>
+        <span>{value}</span>
       </div>
     </div>
   );
